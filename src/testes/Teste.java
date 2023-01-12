7619b64e74dc3169aa1c53ee103ec2492e137a92
GamaGustavo <gustavo98ag@gmail.com>
2023-01-12 08:48:17 -0300

    Criação do pacote e classe de testes onde sera realizado os testes

--- src/testes/Teste.java
+++ src/testes/Teste.java
@@ -1,11 +1,22 @@
 package testes;
 
-import static Avaliacao2.AlgoritmoGenetico.Selecao.newSelecao;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Teste {
     public static void main(String[] args) {
-        System.out.println(newSelecao());
-        System.out.println(newSelecao());
-        System.out.println(newSelecao());
+        System.out.println("A>B>C>D");
+        Map<String,Integer> testMap = new HashMap<>();
+        testMap.put("A",1);
+        testMap.put("B",2);
+        testMap.put("C",2);
+        System.out.println(testMap);
+        System.out.println("A>C>B>D");
+        testMap.clear();
+        testMap.put("A",2);
+        testMap.put("C",2);
+        testMap.put("B",2);
+        System.out.println(testMap);
     }
 }
