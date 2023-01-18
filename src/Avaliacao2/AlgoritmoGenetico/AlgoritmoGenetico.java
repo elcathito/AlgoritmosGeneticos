d645f7d2f3da012149a6782adb1a9e7ae81991a6
GamaGustavo <gustavo98ag@gmail.com>
2023-01-18 13:17:35 -0300

    Resultados na tela

--- src/Avaliacao2/AlgoritmoGenetico/AlgoritmoGenetico.java
+++ src/Avaliacao2/AlgoritmoGenetico/AlgoritmoGenetico.java
@@ -6,52 +6,40 @@ import Avaliacao2.Grafo3.Grafo;
 
 import java.util.*;
 
-import static Avaliacao2.AlgoritmoGenetico.Prints.informacoesInicais;
 import static Avaliacao2.AlgoritmoGenetico.recombinacao.Recombinacao.newRecombinacao;
 import static Avaliacao2.AlgoritmoGenetico.selecao.Selecao.newSelecao;
 
 public class AlgoritmoGenetico {
-    private final int tamanhoPopulacao;
-    private final double porcentagemMutacao;
-    private final int nrGeracoes;
     private final ArrayList<IndividuoAbs> populacao;
     private final Grafo grafo;
     private final Random random;
 
-    public AlgoritmoGenetico(int tamanhoPopulacao, double porcentagemMutacao, int nrGeracoes, int nrVertices, int maxDist) {
-        this.tamanhoPopulacao = tamanhoPopulacao;
-        this.porcentagemMutacao = porcentagemMutacao;
-        this.nrGeracoes = nrGeracoes;
+    public AlgoritmoGenetico(Grafo grafo) {
         this.populacao = new ArrayList<>();
-        this.grafo = new Grafo(nrVertices, maxDist);
+        this.grafo = grafo;
         this.random = new Random();
-        System.out.println(grafo);
-        System.out.println();
     }
 
-    public void start(int verticeOrigem, int verticeDestino,int nrDeRodadas,int nrDeCompetidore,double pocentElite){
+    public String start(int verticeOrigem, int verticeDestino, int nrDeCompetidore, double pocentElite, int tamanhoPopulacao, double porcentagemMutacao, int nrGeracoes) {
+        StringBuilder builder = new StringBuilder();
         long startTime = System.currentTimeMillis();
         populacao.clear();
-        informacoesInicais(nrGeracoes, tamanhoPopulacao, porcentagemMutacao);
-
-        System.out.println("START...");
-
+//        informacoesInicais(nrGeracoes, tamanhoPopulacao, porcentagemMutacao);
+//        System.out.println("START...");
         for (int index = 0; index < tamanhoPopulacao; index++) {
             populacao.add(new IndividuoAd(grafo, verticeOrigem, verticeDestino));
         }
-        System.out.println("POPULACAO INICIAL ADICIONADA...");
+        /*System.out.println("POPULACAO INICIAL ADICIONADA...");
         populacao.forEach(System.out::println);
         System.out.println("\n>> -------------------------------------------------------------------------------------------------- <<\n");
-
-        System.out.println("PROCESSANDO AS GERACOES...");
+        System.out.println("PROCESSANDO AS GERACOES...");*/
         List<IndividuoAbs> novaPop = populacao;
         for (int geracao = 0; geracao < nrGeracoes; geracao++) {
-            List<IndividuoAbs> elite = newSelecao().elitismo(tamanhoPopulacao,novaPop, pocentElite);
-            List<IndividuoAbs> torneio = newSelecao().selecaoPorTorneio(tamanhoPopulacao,novaPop, nrDeCompetidore, false);
+            List<IndividuoAbs> elite = newSelecao().elitismo(tamanhoPopulacao, novaPop, pocentElite);
+            List<IndividuoAbs> torneio = newSelecao().selecaoPorTorneio(tamanhoPopulacao, novaPop, nrDeCompetidore, false);
             novaPop.clear();
             novaPop.addAll(elite);
             novaPop.addAll(torneio);
-
             List<IndividuoAbs> novaPopFilho = new ArrayList<>();
             int sizeNovaPop = novaPop.size();
 
@@ -60,22 +48,27 @@ public class AlgoritmoGenetico {
                 IndividuoAd pai2 = (IndividuoAd) novaPop.get(random.nextInt(sizeNovaPop));
                 newRecombinacao().umPonto(pai1.getCromossomo(), pai2.getCromossomo(), novaPopFilho);
             }
-
             novaPop.addAll(novaPopFilho);
-
-            System.out.println("Geração nr" + geracao);
+            /*System.out.println("Geração nr" + geracao);
             System.out.println("Melhor individo da geração");
-            System.out.println(novaPop.get(0));
-
-
-
+            System.out.println(novaPop.get(0));*/
         }
 
-        System.out.println("\n>> -------------------------------------------------------------------------------------------------- <<\n");
+//        System.out.println("\n>> -------------------------------------------------------------------------------------------------- <<\n");
         long endTime = System.currentTimeMillis();
         long timeElapsed = endTime - startTime;
-
-        System.out.println("Execution time in milliseconds: " + timeElapsed);
+        return builder.append("Resultado\n")
+                .append("Nr de gerações: ").append(nrGeracoes).append("\n")
+                .append("Taxa de mutação: ").append(porcentagemMutacao).append("%\n")
+                .append("Porcentagem da elite: ").append(pocentElite).append("%\n")
+                .append("Nr de competidores: ").append(nrDeCompetidore).append(" individos\n")
+                .append("Tamanho da População: ").append(tamanhoPopulacao).append(" individos\n")
+                .append("Vertice de origem: ").append(verticeOrigem).append("\n")
+                .append("Vertice de destino: ").append(verticeDestino).append("\n")
+                .append("Tempo de execução: ").append(timeElapsed).append("mile segundos\n")
+                .append("Melhor Individo: ").append(novaPop.get(0)).append("\n")
+                .append("Nr de gerações até a convergencia: ").append(nrGeracoes).append("\n").toString();
+//        System.out.println("Execution time in milliseconds: " + timeElapsed);
     }
 
 
